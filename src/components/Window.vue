0d2b36bf14187d94f13301dd8c043724aba34e8d
Zulaikha Zakiullah <zzakiull@uwaterloo.ca>
2021-03-28 17:27:28 -0400

    Add base files

---
+++ src/components/Window.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Window",
+    props: {
+        
+    }    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/icons.js
@@ -0,0 +1,42 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+    faFolder,
+    faTimes,
+    faTrash,
+    faEnvelope,
+    faSearch,
+    faBatteryThreeQuarters,
+    faPowerOff,
+    faPaperPlane
+} from '@fortawesome/free-solid-svg-icons';
+import {
+    faWindowMinimize,
+    faWindowMaximize,
+    faWindowRestore,
+    faFilePdf,
+    faFileWord
+} from '@fortawesome/free-regular-svg-icons';
+import {
+    faLinkedinIn,
+    faGithub,
+    faDiscord
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+    faFolder,
+    faTimes,
+    faTrash,
+    faEnvelope,
+    faSearch,
+    faBatteryThreeQuarters,
+    faPowerOff,
+    faPaperPlane,
+    faWindowMinimize,
+    faWindowMaximize,
+    faWindowRestore,
+    faFilePdf,
+    faFileWord,
+    faLinkedinIn,
+    faGithub,
+    faDiscord
+);
