8ab4f4a7eefbef4b132cab913bd3b7c2b2ed71d9
Zulaikha Zakiullah <zzakiull@uwaterloo.ca>
2021-03-29 19:24:17 -0400

    Configure variables and add to window

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Window title="about"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Window } from './components/index'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Window
   }
 }
 </script>
--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,41 +1,54 @@
 <template>
     <div class="window">
-        <div class="title-bar">
+        <div class="window__title-bar">
             {{ title }}
         </div>
         <div>
 
         </div>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
 </template>
 
 <script>
+import HelloWorld from './HelloWorld';
+
 export default {
     name: "Window",
+    components: {
+        HelloWorld
+    },
     data: function () {
         return {
-
+            isMaximized: false
+        }
+    },
+    methods: {
+        greet: function (event) {
+            event.preventDefault()
+            alert(this.isMaximized)
         }
     },
     props: {
-        title: String,
-        isMaximized: Boolean
+        title: String
     }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '~@styles/_mixins.scss';
-@import '~@styles/_variables.scss';
+@import '../styles/_mixins.scss';
+@import '../styles/_variables.scss';
 
 .window {
 
     &__title-bar {
-
+        display: block;
+        width: 100%;
     }
 
     &__content {
-
+        display: block;
+        width: 100%;
     }
 
     &__minimize-btn {
@@ -53,5 +66,21 @@ export default {
     &__close-btn {
 
     }
+}
+
+@include media($SM_SCREEN) {
+
+}
+
+@include media($MD_SCREEN) {
+
+}
+
+@include media($LG_SCREEN) {
+
+}
+
+@include media($XL_SCREEN) {
+
 }
 </style>
